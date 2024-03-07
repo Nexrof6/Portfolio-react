7aaa7621c4ffd094e2434d0101cc155d7b2929fd
sanjaysagar12 <sanjaysagar.main@gmail.com>
2024-03-07 21:55:32 +0530

    Implimented Simple Parallax

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import Page_Home from "./templates/Page_Home";
-
+import "./index.js";
 function App() {
   return <Page_Home></Page_Home>;
 }
--- src/templates/Page_Home.tsx
+++ src/templates/Page_Home.tsx
@@ -6,16 +6,18 @@ function Page_Home() {
       <NavBar></NavBar>
       <main>
         <div>
-          <h1 className="text-center parallax-div">DIV 1</h1>
+          <h1 className="text-center parallax-div-1" id="parallax">
+            DIV 1
+          </h1>
         </div>
         <div>
-          <h1 className="text-center parallax-div">DIV 2</h1>
+          <h1 className="text-center parallax-div-2">DIV 2</h1>
         </div>
         <div>
-          <h1 className="text-center parallax-div">DIV 3</h1>
+          <h1 className="text-center parallax-div-3">DIV 3</h1>
         </div>
         <div>
-          <h1 className="text-center parallax-div">DIV 4</h1>
+          <h1 className="text-center parallax-div-4">DIV 4</h1>
         </div>
       </main>
     </>
