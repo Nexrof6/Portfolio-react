2290db9f71a695ecb8eb44cd870f8c0d0133aa1f
sanjaysagar12 <sanjaysagar.main@gmail.com>
2024-03-09 17:10:49 +0530

    Added Text Animation

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,5 +1,12 @@
 function Home() {
-  return <h1>Home page</h1>;
+  return (
+    <div>
+      <h1 className="slide-in-text-left">Left to Right Animation</h1>
+      <h1 className="slide-in-text-right">Right to Left Animation</h1>
+      <h1 className="slide-in-text-top">Top to Bottom Animation</h1>
+      <h1 className="slide-in-text-bottom">Bottom to Top Animation</h1>
+    </div>
+  );
 }
 
 export default Home;
