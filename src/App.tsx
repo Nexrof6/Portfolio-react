f3361133f419d9df38916943b0338e88237a00f6
sanjaysagar12 <sanjaysagar.main@gmail.com>
2024-03-09 19:42:18 +0530

    Multi Image Parallax

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import Test from "./components/pages/Test.js";
 import Home from "./components/pages/Home.js";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.js";
+import ParallaxTest from "./components/pages/ParallaxTest.js";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/test" element={<Test />} />
+        <Route path="/parallax" element={<ParallaxTest />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/pages/ParallaxTest.tsx
@@ -0,0 +1,86 @@
+import { ParallaxBanner, ParallaxProvider } from "react-scroll-parallax";
+
+import bg1 from "/image/bg-1.jpg";
+import bg2 from "/image/bg-2.png";
+import bg3 from "/image/bg-3.png";
+import bg4 from "/image/bg-4.png";
+import bg5 from "/image/bg-5.png";
+import "/src/index.css";
+console.log("bg1:", bg1);
+function ParallaxTest() {
+  return (
+    <main>
+      <ParallaxProvider>
+        <header>
+          <ParallaxBanner
+            className="banner"
+            layers={[
+              {
+                image: bg1,
+                translateY: [0, 50],
+                shouldAlwaysCompleteAnimation: true,
+                expanded: false,
+              },
+              {
+                image: bg2,
+                translateY: [5, 45],
+
+                shouldAlwaysCompleteAnimation: true,
+                expanded: false,
+              },
+              {
+                children: (
+                  <div
+                    style={{
+                      color: "white",
+                      position: "absolute",
+                      top: "30%",
+                      left: "50%",
+                      transform: "translate(-50%, -50%)",
+                    }}
+                  >
+                    <h1
+                      style={{
+                        fontSize: "100px",
+                      }}
+                    >
+                      Nexrof
+                    </h1>
+                  </div>
+                ),
+                // translateY: [15, 25],
+                translateY: [5, 45],
+                shouldAlwaysCompleteAnimation: true,
+                expanded: false, //Set the amount to 0 to prevent any parallax effect on the text
+              },
+              {
+                image: bg3,
+                translateY: [10, 30],
+
+                shouldAlwaysCompleteAnimation: true,
+                expanded: false,
+              },
+
+              {
+                image: bg4,
+                translateY: [15, 25],
+
+                shouldAlwaysCompleteAnimation: true,
+                expanded: false,
+              },
+              {
+                image: bg5,
+                translateY: [20, 20],
+
+                shouldAlwaysCompleteAnimation: true,
+                expanded: false,
+              },
+            ]}
+          />
+        </header>
+      </ParallaxProvider>
+    </main>
+  );
+}
+
+export default ParallaxTest;
