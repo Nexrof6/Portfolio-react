5b49781e0659e79d77efe8464bb77e65ebd25617
sanjaysagar12 <sanjaysagar.main@gmail.com>
2024-03-11 01:12:00 +0530

    Parallax Completed

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -4,12 +4,15 @@ function Home() {
   return (
     <>
       <NavBar></NavBar>
-      <main className="md-5">
-        <div>
-          <h1 className="slide-in-text-left">Left to Right Animation</h1>
-          <h1 className="slide-in-text-right">Right to Left Animation</h1>
+      <main>
+        <div className="container-flex pt-5">
+          <h3 className="slide-in-text-left ">NEXROF</h3>
+          {/* <h1 className="slide-in-text-right mt-5">Right to Left Animation</h1>
           <h1 className="slide-in-text-top">Top to Bottom Animation</h1>
-          <h1 className="slide-in-text-bottom">Bottom to Top Animation</h1>
+          <h1 className="slide-in-text-bottom">Bottom to Top Animation</h1> */}
+        </div>
+        <div className="container-flex pt-5">
+          <h3 className="slide-in-text-left ">THE FUTURE REVOLUTION</h3>
         </div>
       </main>
     </>
--- src/components/pages/ParallaxTest.tsx
+++ src/components/pages/ParallaxTest.tsx
@@ -4,85 +4,100 @@ import bg1 from "/image/bg-1.png";
 import bg2 from "/image/bg-2.png";
 import bg3 from "/image/bg-3.png";
 import bg4 from "/image/bg-4.png";
-import bg5 from "/image/bg-5.png";
+// import bg5 from "/image/bg-5.png";
 import "/src/index.css";
+import NavBar from "../templates/NavBar";
 console.log("bg1:", bg1);
 function ParallaxTest() {
   return (
-    <main className="parallax">
-      <ParallaxProvider>
-        <header>
-          <ParallaxBanner
-            className="banner"
-            layers={[
-              {
-                image: bg1,
-                translateY: [0, 50],
-                shouldAlwaysCompleteAnimation: true,
-                expanded: false,
-              },
-              {
-                image: bg2,
-                translateY: [0, 45]},
-              // {
-              //   image: bg2,
-              //   translateY: [5, 45],
+    <>
+      <NavBar></NavBar>
+      <main>
+        <div className="row">
+          <div className=" col-md order-md-1">
+            <ParallaxProvider>
+              <header>
+                <ParallaxBanner
+                  className="banner"
+                  layers={[
+                    {
+                      children: (
+                        <div
+                          style={{
+                            color: "white",
+                            position: "absolute",
+                            top: "20%",
+                            left: "50%",
+                            transform: "translate(-50%, -50%)",
+                          }}
+                        >
+                          <h1
+                            style={{
+                              fontSize: "100px",
+                            }}
+                          >
+                            Nexrof
+                          </h1>
+                        </div>
+                      ),
+                      // translateY: [15, 25],
+                      translateY: [10, 50],
+                      shouldAlwaysCompleteAnimation: true,
+                      expanded: false, //Set the amount to 0 to prevent any parallax effect on the text
+                    },
+                    {
+                      image: bg1,
+                      translateY: [15, -100],
+                      translateX: [10, 0],
+                      shouldAlwaysCompleteAnimation: true,
+                      expanded: false,
+                    },
+                    {
+                      image: bg2,
+                      translateY: [15, 35],
 
-              //   shouldAlwaysCompleteAnimation: true,
-              //   expanded: false,
-              // },
-              {
-                children: (
-                  <div
-                    style={{
-                      color: "white",
-                      position: "absolute",
-                      top: "10%",
-                      left: "50%",
-                      transform: "translate(-50%, -50%)",
-                    }}
-                  >
-                    <h1
-                      style={{
-                        fontSize: "100px",
-                      }}
-                    >
-                      Nexrof
-                    </h1>
-                  </div>
-                ),
-                // translateY: [15, 25],
-                translateY: [10, 45],
-                shouldAlwaysCompleteAnimation: true,
-                expanded: false, //Set the amount to 0 to prevent any parallax effect on the text
-              },
-              {
-                image: bg3,
-                translateY: [0, 30],
+                      shouldAlwaysCompleteAnimation: true,
+                      expanded: false,
+                    },
+                    {
+                      image: bg3,
+                      translateY: [-5, 20],
 
-                shouldAlwaysCompleteAnimation: true,
-                expanded: false,
-              },
+                      shouldAlwaysCompleteAnimation: true,
+                      expanded: false,
+                    },
 
-              {
-                image: bg4,
-                translateY: [5, 0],
+                    {
+                      image: bg4,
+                      translateY: [10, 20],
 
-                shouldAlwaysCompleteAnimation: true,
-                expanded: false,
-              },
-              {
-                image: bg5,
-                translateY: [0, -20],
+                      shouldAlwaysCompleteAnimation: true,
+                      expanded: false,
+                    },
+                    // {
+                    //   image: bg5,
+                    //   translateY: [0, -20],
 
-                shouldAlwaysCompleteAnimation: true,
-                expanded: false,
-              },
-            ]}
-          />
-        </header>
-      </ParallaxProvider>
-    </main>
+                    //   shouldAlwaysCompleteAnimation: true,
+                    //   expanded: false,
+                    // },
+                  ]}
+                />
+              </header>
+            </ParallaxProvider>
+          </div>
+          <div className="col-md ">
+            <h1
+              className=" container text-justify "
+              style={{ marginTop: "30%" }}
+            >
+              NEXUS REVOLUTION OF THE FUTURE
+            </h1>
+          </div>
+        </div>
+        <div className="parallax"></div>
+      </main>
+    </>
   );
 }
 
