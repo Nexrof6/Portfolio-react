08ddc3d51815328daa2c5e652a3ab6e016ca607c
sanjaysagar12 <sanjaysagar.main@gmail.com>
2024-03-11 14:56:40 +0530

    Created Component ParallaxMultiLayer.tsx

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,3 +1,4 @@
+import ParallaxMultiLayer from "../effects/Parallax/ParallaxMultiLayer";
 import NavBar from "../templates/NavBar";
 
 function Home() {
@@ -5,14 +6,11 @@ function Home() {
     <>
       <NavBar></NavBar>
       <main>
-        <div className="container-flex pt-5">
-          <h3 className="slide-in-text-left ">NEXROF</h3>
-          {/* <h1 className="slide-in-text-right mt-5">Right to Left Animation</h1>
-          <h1 className="slide-in-text-top">Top to Bottom Animation</h1>
-          <h1 className="slide-in-text-bottom">Bottom to Top Animation</h1> */}
-        </div>
-        <div className="container-flex pt-5">
-          <h3 className="slide-in-text-left ">THE FUTURE REVOLUTION</h3>
+        <div className="container">
+          <ParallaxMultiLayer>
+            {" "}
+            NEXUS REVOLUTION OF THE FUTURE
+          </ParallaxMultiLayer>
         </div>
       </main>
     </>
